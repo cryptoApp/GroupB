afe621aef67300b6aecd73ba0ecf32670fbdc3a9
Christian <christian926@homtail.com>
2015-05-09 14:59:12 +0200

    uppdatering 2015-05-09/Christopher

--- app/src/main/java/CustomAlgorithm/Main.java
+++ app/src/main/java/CustomAlgorithm/Main.java
@@ -4,6 +4,9 @@ package CustomAlgorithm;
  * Created by Christian on 2015-05-08.
  */
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  *
  * @author Christian
@@ -14,7 +17,7 @@ package CustomAlgorithm;
     KeyGenerator keyGen;
     EndAlgorithm endAlg;
 
-    public Main(String keyFromUser,String password) throws Exception {
+    public Main(String keyFromUser,String password,Context c) throws Exception {
 
             encGen = new EncryptGenerator();
             keyGen = new KeyGenerator();
@@ -23,9 +26,11 @@ package CustomAlgorithm;
             encGen.setKey(key);
 
             String pass = new String(encGen.encryptPassword(password.getBytes())); //Get the AES encrypted password
-            endAlg = new EndAlgorithm(pass, keyFromUser); //Encrypt the password in our engine
-            pass = endAlg.finalAlgorithm();
 
+            endAlg = new EndAlgorithm(pass, keyFromUser);
+            //Encrypt the password in our engine
+            pass = endAlg.finalAlgorithm();
+            Toast.makeText(c,pass,Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/admir/ourproject/MainActivity.java
+++ app/src/main/java/com/example/admir/ourproject/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.admir.ourproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import CustomAlgorithm.Main;
 
@@ -13,7 +19,8 @@ import CustomAlgorithm.Main;
 public class MainActivity extends Activity {
     EditText length;
     Main chryptoAlgorithm;
-
+    String imgKey;
+    Button encPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +28,104 @@ public class MainActivity extends Activity {
         length = (EditText) findViewById(R.id.passwordLength);
         length.setGravity(Gravity.CENTER);
         length.getBackground().setAlpha(50);
-
-
+        imgKey = "nokey";
+        encPass = (Button) findViewById(R.id.encryptedPassword);
     }
 
     public void textClick(View v) {
-        Toast.makeText(this, "Här kommer det att gå att välja key genom att skriva in vad man vill!", Toast.LENGTH_LONG).show();
+       openDialogForInput();
     }
 
-    public void imageClick(View v) {
-        Toast.makeText(this, "Här kommer det komma upp bilder på FB, Instagram, Twitter, Pornhub.", Toast.LENGTH_LONG).show();
 
+    public void imageClick(View v) {
+        openDialogForImageKey();
     }
 
     public void getPassword(View v) {
         Toast.makeText(this, "Här får man upp den krypterade lösenordet!", Toast.LENGTH_LONG).show();
+
+    }
+
+    private void openDialogForInput() {
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View v = factory.inflate(R.layout.alertwithtwoedits, null);
+        final EditText key = (EditText) v.findViewById(R.id.editText);
+        final EditText pass = (EditText) v.findViewById(R.id.editText2);
+        key.setHint("Write your key");
+        pass.setHint("Write your password");
+        alert.setView(v);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String keyString = key.getText().toString();
+                String passString = pass.getText().toString();
+                try {
+                    chryptoAlgorithm = new Main(keyString, passString,MainActivity.this);
+
+                }catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),"Error occured.." + e,Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                });
+        alert.show();
+
+    }
+
+    private void openDialogForImageKey() {
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View v = factory.inflate(R.layout.imagekeydialog, null);
+        final EditText pass = (EditText) v.findViewById(R.id.password);
+        final TextView tv = (TextView) v.findViewById(R.id.textView);
+        tv.setText(imgKey);
+        ImageView fb = (ImageView) v.findViewById(R.id.facebook);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgKey = "Facebook";
+                tv.setText(imgKey);
+            }
+        });
+
+        ImageView ig = (ImageView) v.findViewById(R.id.instagram);
+        ImageView bad = (ImageView) v.findViewById(R.id.badoo);
+        ImageView woff = (ImageView) v.findViewById(R.id.woff);
+        ImageView tind = (ImageView) v.findViewById(R.id.tinder);
+        ImageView twitt = (ImageView) v.findViewById(R.id.twitter);
+        pass.setHint("Write your password");
+        alert.setView(v);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String passString = pass.getText().toString();
+                try {
+                    chryptoAlgorithm = new Main(imgKey, passString,MainActivity.this);
+                }catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),"Error occured.." + e,Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.cancel();
+                    }
+                });
+        alert.show();
 
     }
 }
