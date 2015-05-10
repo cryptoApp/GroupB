bb7173c4deafd204224bf73cdb2e4e531317b593
Admir <admir.muhic@live.se>
2015-05-10 11:55:24 +0200

    Hallå hallå där.

--- app/src/main/java/com/example/admir/ourproject/MainActivity.java
+++ app/src/main/java/com/example/admir/ourproject/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
     StartAlgorithm chryptoAlgorithm;
     String imgKey;
     Button encPass;
+    String hej = "f";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
